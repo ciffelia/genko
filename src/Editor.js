a7ad33b9e90ade267c415181cddf024d04316003
prince0203 <mc.prince.0203@gmail.com>
2017-09-25 20:19:01 +0900

    Move caret to dropped position

--- src/Editor.js
+++ src/Editor.js
@@ -3,9 +3,25 @@ import './Editor.css';
 
 class Editor extends Component {
   // The text pasted or dropped may contains images or styles, so remove them
-  static onContentInserted(e) {
+  static onPaste(e) {
     // Get pasted contents as a plain text
-    const plainText = (e.clipboardData || e.nativeEvent.dataTransfer || null).getData('Text');
+    const plainText = e.clipboardData.getData('Text');
+  
+    document.execCommand('insertText', false, plainText);
+    
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  static onDrop(e) {
+    // Get dropped contents as a plain text
+    const plainText = e.nativeEvent.dataTransfer.getData('Text');
+    
+    // Move caret to dropped position
+    const selection = window.getSelection();
+    const range = document.caretRangeFromPoint(e.clientX, e.clientY);
+    selection.removeAllRanges();
+    selection.addRange(range);
     
     document.execCommand('insertText', false, plainText);
     
@@ -25,8 +41,8 @@ class Editor extends Component {
       <div
         className="Editor"
         contentEditable={true}
-        onPaste={Editor.onContentInserted}
-        onDrop={Editor.onContentInserted}
+        onPaste={Editor.onPaste}
+        onDrop={Editor.onDrop}
         onInput={Editor.onChange}
       />
     );
